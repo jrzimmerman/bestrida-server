87be8bda35f7deb8e6d233d9cd10490df03ecdb1
Justin Zimmerman <zimmy130@gmail.com>
2015-12-07 14:59:36 -0500

    set up basic express server

---
+++ server.js
@@ -0,0 +1,6 @@
+var path = require('path');
+var express = require('express');
+var app = express();
+
+app.use(express.static(path.join(__dirname, 'public')));
+app.listen(process.env.PORT || 8000);
