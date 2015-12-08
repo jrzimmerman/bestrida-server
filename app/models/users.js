fe1bf36446e09d5b04d50a7d8c976569f94dfce6
David Lee <david7lee@gmail.com>
2015-12-08 15:09:32 -0500

    refactored User model to use strava id for _id property

--- app/models/users.js
+++ app/models/users.js
@@ -1,24 +1,29 @@
-var findOrCreate = require('mongoose-findorcreate')
 var mongoose = require('../db');
 
 var userSchema = mongoose.Schema({ 
-  stravaId: { type: Number, required: true },
+  _id: { type: Number, required: true },
   name: { type: String, required: true }
-}).plugin(findOrCreate);
+});
 
 var User = mongoose.model('User', userSchema);
 
 // Helper functions
-module.exports.findOrCreate = function (stravaId, name) {
-  User.findOrCreate(
-    { stravaId: stravaId },
-    { name: name },
-    function (err, user, created) {
-      // 'created' will be true if a new user was created
-      if (err) {
-        console.error(err);
+module.exports = {
+  findOrCreate: function (stravaId, name) {
+    User.findOrCreate(
+      {
+        _id: stravaId
+      },
+      {
+        name: name
+      },
+      function (err, user, created) {
+        // 'created' will be true if a new user was created
+        if (err) {
+          console.error(err);
+        }
+        return user;
       }
-      return user;
-    }
-  );
+    );
+  }
 };
