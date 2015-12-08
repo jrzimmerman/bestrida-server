5e3636acf0eaeee84696500e29db0f370045e2f5
David Lee <david7lee@gmail.com>
2015-12-08 17:06:42 -0500

    refactored User model, can access native mongoose Model methods

--- app/models/users.js
+++ app/models/users.js
@@ -7,27 +7,34 @@ var userSchema = mongoose.Schema({
 
 var User = mongoose.model('User', userSchema);
 
-// Helper functions
-module.exports = {
-  find: function (parameters, cb) {
-    User.find(parameters, cb);
-  },
-  findOrCreate: function (stravaId, name) {
-    User.findOrCreate(
-      {
-        _id: stravaId
-      },
-      {
-        name: name
-      },
-      function (err, user, created) {
-        // 'created' will be true if a new user was created
-        if (err) {
-          console.error(err);
-        }
-        // console.log(user);
-        return user;
-      }
-    );
-  }
+module.exports = User;
+
+module.exports.create = function (stravaId, name) {
+  var newUser = new User({ _id: stravaId, name: name });
+  newUser.save();
 };
+
+// If we need findOrCreate method, can use this code but wouldn't have access to mongoose Model methods
+// module.exports = {
+//   find: function (parameters, cb) {
+//     User.find(parameters, cb);
+//   },
+//   findOrCreate: function (stravaId, name) {
+//     User.findOrCreate(
+//       {
+//         _id: stravaId
+//       },
+//       {
+//         name: name
+//       },
+//       function (err, user, created) {
+//         // 'created' will be true if a new user was created
+//         if (err) {
+//           console.error(err);
+//         }
+//         // console.log(user);
+//         return user;
+//       }
+//     );
+//   }
+// };
