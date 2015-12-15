c408ad21902f740e1b5ed30cbdc435bb8db063c8
Justin Zimmerman <zimmy130@gmail.com>
2015-12-14 23:08:19 -0500

    (feat) add segements to database on lookup
    (feat) store segment calls in database

--- app/models/segments.js
+++ app/models/segments.js
@@ -3,21 +3,119 @@ var mongoose = require('../db');
 var segmentSchema = mongoose.Schema({ 
   _id: { type: Number, required: true },
   name: { type: String, required: true },
-  activityType: { type: String, required: true }
+  activityType: { type: String, required: true },
+  distance: { type: Number, required: true },
+  averageGrade: {type: Number, required: true },
+  startLatLng: { type: [mongoose.Schema.Types.Mixed], default: [], required: true },
+  endLatLng: { type: [mongoose.Schema.Types.Mixed], default: [], required: true },
+  climbCategory: {type: Number, required: true },
+  city: { type: String, required: true },
+  state: { type: String, required: true },
+  country: { type: String, required: true },
+  totalElevationGain: {type: Number, required: true }
 });
+
 
 var Segment = mongoose.model('Segment', segmentSchema);
 
 module.exports = Segment;
 
-module.exports.create = function (segmentId, name, activityType) {
+module.exports.create = function (segmentId, name, activityType, distance,
+                                  averageGrade, startLatLng, endLatLng, climbCategory,
+                                  city, state, country, totalElevationGain) {
   var newSegment = new Segment({
     _id: segmentId,
     name: name,
-    activityType: activityType
+    activityType: activityType,
+    distance: distance,
+    averageGrade: averageGrade,
+    startLatLng: startLatLng,
+    endLatLng: endLatLng,
+    climbCategory: climbCategory,
+    city: city,
+    state: state,
+    country: country,
+    totalElevationGain: totalElevationGain
   });
   newSegment.save();
 };
+
+
+
+////////////////////////////////////////////////////
+///
+////////////////////////////////////////////////////
+// module.exports.registerSegment = function (segment, callback) {
+//   // Check if segment exists in db
+//   Segment.find({ _id: segment.id })
+//   .then(function (segmentsArray) {
+//     // If segment exists, just refresh token
+//     if (segmentsArray[0]){
+//       refreshSegment(segment, callback);
+//     } else {
+//       // Else if segment doesn't exist in db, save them to db
+//       saveSegment(segment, callback);
+//     }
+//   }, function (err) {
+//     console.error('Error retrieving segment:', err);
+//   });
+// };
+
+module.exports.saveSegment = function (segment, callback) {
+  var newSegment = new Segment({
+    _id: segment.id,
+    name: segment.name,
+    activityType: segment.activity_type,
+    distance: segment.distance,
+    averageGrade: segment.average_grade,
+    startLatLng: segment.start_latlng,
+    endLatLng: segment.end_latlng,
+    climbCategory: segment.climb_category,
+    city: segment.city,
+    state: segment.state,
+    country: segment.country,
+    totalElevationGain: segment.total_elevation_gain
+  });
+
+  newSegment.save(function (err, segment) {
+    if (err) {
+      console.error('Error saving segment:', err);
+      callback(err);
+    } else {
+      console.log('Segment saved!', segment);
+      callback(null, segment);
+    }
+  });
+};
+
+// function refreshSegment (segment, callback) {
+//   Segment.update(
+//     { _id: segment.id },
+//     {
+//       name: segment.name,
+//       activityType: segment.activityType,
+//       distance: segment.distance,
+//       averageGrade: segment.averageGrade,
+//       startLatLng: segment.startLatLng,
+//       endLatLng: segemnt.endLatLng,
+//       climbCategory: segment.climbCategory,
+//       city: segment.city,
+//       state: segment.state,
+//       country: segment.country,
+//       totalElevationGain: segment.totalElevationGain
+//     },
+//     function (err, res) {
+//       if (err) {
+//         console.error('Error refreshing segment:', err);
+//         callback(err);
+//       }
+//       console.log('Successfully refreshed segment:', res);
+//       callback(null, res);
+//     });
+// }
+
+
+
 
 // If we need findOrCreate method, can use this code but wouldn't have access to mongoose Model methods
 // module.exports = {
