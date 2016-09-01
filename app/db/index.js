8cd73dcf1bf6a85e42fa69716ce5a01d05905b33
Justin Zimmerman <jzimmerman@redventures.com>
2016-09-01 15:18:15 -0400

    fix db

--- app/db/index.js
+++ app/db/index.js
@@ -1,7 +1,7 @@
 var mongoose = require('mongoose');
 mongoose.Promise = require('bluebird');
-// mongoose.connect(process.env.MONGODB_URI);
-mongoose.connect('mongodb://localhost:27017/bestrida');
+mongoose.connect(process.env.MONGODB_URI);
+// mongoose.connect('mongodb://localhost:27017/bestrida');
 var db = mongoose.connection;
 
 db.on('error', console.error.bind(console, 'connection error:'));
